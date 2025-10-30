30ff1c5b6b0a6f8fc61d679d2b5aa0f66548d923
matthew-mills182 <mills.matthew182@gmail.com>
2025-10-30 19:58:39 +0000

    defined which files will go to which folders

--- organizer.py
+++ organizer.py
@@ -1,2 +1,14 @@
 import os
+import shutil
 
+# Establish folder to watch
+downloads = r"/mnt/c/Users/Matthew/Downloads"
+
+folders = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg", ".webp"],
+    "PDFs": [".pdf"],
+    "Documents": [".doc", ".docx", ".txt", ".ppt", ".pptx", ".xls", ".xlsx", ".odt", ".rtf"],
+    "Videos": [".mp4", ".mov", ".avi", ".mkv", ".flv", ".wmv", ".webm"],
+    "Audio": [".mp3", ".wav", ".flac", ".aac", ".ogg", ".m4a"],
+    "Archives": [".zip", ".rar", ".7z", ".tar", ".gz", ".bz2"]
+}
